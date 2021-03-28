5bb21c0b9d97e887a4b89e6e63a53a254774e400
Jacky Tong <tongjac@yahoo.com>
2021-03-27 22:49:18 -0700

    Added credit line at the top of script

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Written by Tongjac @ Github
+// Thanks for using!
+
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
