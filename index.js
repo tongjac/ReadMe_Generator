50e3e855ff4cb169136e02c9bfb837834a1f90c8
Jacky Tong <tongjac@yahoo.com>
2021-03-27 20:47:59 -0700

    Added questions and reorganized index.js. Markdown generator also formatted properly.

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
-var inquirer = require("inquirer");
-var fs = require("fs");
-
-  .prompt([
-    /* Pass your questions in here */
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Describe your project in a short paragraph.",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Enter installation instructions.",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Enter usage information.",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Enter contribution guidelines.",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "Enter test instructions.",
+    name: "test",
+  },
+  {
+    type: "input",
+    message: "Name your license.",
+    name: "license",
+  },
+  {
+    type: "input",
+    message: "Enter your GitHub username.",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "Enter your E-mail Address.",
+    name: "email",
+  },
+];
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -29,7 +62,23 @@ function writeToFile(fileName, data) {
 }
 
 // function to initialize program
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      generateMarkdown(answers);
+      fs.writeFile("README.md", generateMarkdown(return));
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        console.log("Prompt couldn't be rendered in the current environment");
+      } else {
+        console.log(
+          "Sorry, something went wrong. Please run node index.js again."
+        );
+      }
+    });
+}
 
 // function call to initialize program
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,26 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${data.description}
+  #
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contribution Instructions
+  ${data.contribution}
+
+  ## Testing
+  ${data.test}
+
+  ## Developer's Github
+  ${data.github}
+
+  ## Contact Info
+  ${data.email}
 
 `;
 }
