ba5aa7fe5b02f0db220671af7a93436bca7c5e67
Jacky Tong <tongjac@yahoo.com>
2021-03-27 20:16:12 -0700

    Propagated README for project, added fs write and append to program

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# ReadMe_Generator
-Quickly and easily generate a README file!
+# README Generator
+
+#
+
+## When to use the Generator
+
+This README Generator is meant to be executed across a command line. I recommend Gitbash primarily but any that will allow you to `node index.js` will be adequate. Make sure to run this with file creation permissions as it will attempt to generate a text file.
+
+## Packages / Technologies Used
+
+- Node.js
+- Javascript
+- Inquirer (npm package)
+
+## How to use
+
+- Clone this repository.
+- Run `npm install` in the root directory. Wait for packages to download (<2 minutes).
+- Open a terminal inside the root directory.
+- Run `node server.js` and follow the prompts that pop up in the console.
+- Hold out hands and receive bacon.
--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-// array of questions for user
-const questions = [
+var inquirer = require("inquirer");
+var fs = require("fs");
 
-];
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
+// array of questions for user
+const questions = [];
 
 // function to write README file
 function writeToFile(fileName, data) {
+  fs.appendFile(fileName, data, function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log("Your new README has been generated!");
+  });
 }
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
